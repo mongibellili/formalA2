e26c86a4ba0a1117c0f3f512c7ff8e1caa379cd3
mongibellili <bellilimongi@gmail.com>
2024-07-02 12:53:05 +0200

    remove https

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,6 @@ using Documenter, formalA2
 
 makedocs(sitename="My Documentation")
 deploydocs(
-    repo   = "https://github.com/mongibellili/formalA2.git",
+    repo   = "github.com/mongibellili/formalA2.git",
     branch = "gh-pages",
 )
